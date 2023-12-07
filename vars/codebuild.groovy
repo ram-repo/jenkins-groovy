4994716dcaf56a3c76b85f5a1ac05e5ab25ac642
ram-repo <ramjagadeesh2205@gmail.com>
2023-12-08 00:07:49 +0530

    Create codebuild.groovy

---
+++ vars/codebuild.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent { label 'buildtool' }
+    parameters {
+        choice(name: 'GOAL', choices: ['compile', 'package', 'clean package'])
+    }
+    stages {
+        stage('Build the Code and sonarqube-analysis') {
+            steps {
+//withSonarQubeEnv('sonar') here name will which integrated in jenkins configuration
+                withSonarQubeEnv('sonar') {
+                    sh script: "mvn ${params.GOAL} sonar:sonar"
+                }
+            }
+        }
+        stage('reporting') {
+            steps {
+                junit testResults: 'target/surefire-reports/*.xml'
+            }
+        }
+    }
+}
