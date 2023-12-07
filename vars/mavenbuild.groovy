a343107d1877bf617008b702b84aeae7cf46968e
ram-repo <ramjagadeesh2205@gmail.com>
2023-12-07 23:28:07 +0530

    Create mavenbuild.groovy

---
+++ vars/mavenbuild.groovy
@@ -0,0 +1,21 @@
+def call () {
+    pipeline {
+      agent {
+          label 'build-agent'
+      }
+      stages {
+          stage('Build') {
+              steps {
+                  sh "mvn clean package"
+              }
+  
+              post {
+                  success {
+                      junit '**/target/surefire-reports/*.xml'
+                      archiveArtifacts 'target/*.jar'
+                  }
+              }
+          }
+      }
+  }
+}
